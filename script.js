f4a22b721cd8100bc2af2e5f807309cfd17a9f86
shveta12345 <102569041+shveta12345@users.noreply.github.com>
2022-07-31 01:23:57 -0500

    Add CSV

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var PrimaryTypeArray = [];
 var DateArray = [];
 var months = { "01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June",
@@ -7,37 +5,34 @@ var months = { "01": "January", "02": "February", "03": "March", "04": "April", 
 var LocationDescriptionArray = [];
 var crimeLocation = [];
 var crimesType = [];
-var chosenField = "Restaurant";
-
+var chosenField = "THEFT";
 function init() {
-    d3.csv("https://raw.githubusercontent.com/shveta12345/shveta12345.github.io/main/data.csv").then(function(data) {
+   d3.csv("https://raw.githubusercontent.com/shveta12345/shveta12345.github.io/main/data.csv").then(function(data) {
         data.forEach(function(d) {
-            PrimaryTypeArray.push(d.FacilityType);
+            PrimaryTypeArray.push(d.PrimaryType);
             var arr = d.Date.split("/");
             var arr1 = d.Date.split(" ");
             var arr2 = arr1[1].split(":");
             DateArray.push(months[arr[0]]);
             LocationDescriptionArray.push(d.LocationDescription);
-            crimeLocation.push([d.FacilityType, d.LocationDescription]);
-            crimesType.push(d.FacilityType)
+            crimeLocation.push([d.PrimaryType, d.LocationDescription]);
+            crimesType.push(d.PrimaryType)
         });
     }); 
-    d3.select(".dropdown").insert("h4").text("Select facility:");
-    d3.select(".dropdown").insert("select").classed("facility",true);
+    d3.select(".dropdown").insert("h4").text("Select crime:");
+    d3.select(".dropdown").insert("select").classed("crime",true);
     d3.select(".dropdown").classed("hidden",true);
     d3.select(".dropdown").insert("span").html('<input type="button" value="Sort this graph" onclick="sort()"/>');
     d3.select("#scene0").insert("h2").text("Introduction");
-    d3.select("#scene0").insert("span").text("This narrative visualization shows Food Inspection at Illinois");
-    // d3.select("#scene0").insert("span").text("Data source is from City of Chicago portal and the data is from 01/01/2010 till now");
+    d3.select("#scene0").insert("span").text("This narrative visualization shows crimes occured in Chicago since 2020. ");
+    d3.select("#scene0").insert("span").text("Data source is from City of Chicago portal and the data is 01/01/2001 to 12/31/2020");
+    d3.select("#scene0").insert("span").html('<a target="_blank" href="https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-Present/ijzp-q8t2/">Link to Chicago Reported Crimes dataset</a>');
     d3.select("#scene0").insert("br");
     d3.select("#scene0").insert("br");
-    d3.select("#scene0").insert("span").html('<a target="_blank" href="https://data.cityofchicago.org/Health-Human-Services/Food-Inspections/4ijn-s7e5/data">Link to Food Inspection at Illinois dataset</a>');
-    d3.select("#scene0").insert("br");
-    d3.select("#scene0").insert("br");
-    d3.select("#beginText").insert("span").text("Click 'Food Inspection Graph' button will allow you to move through different scenes");
+    d3.select("#beginText").insert("span").text("Click 'Chicago Crime Scenes' button will allow you to move through different scenes");
     d3.select("#beginText").insert("br");
     d3.select("#beginText").insert("br");
-    d3.select("#beginText").insert("span").text("> Shveta Shah <");
+    d3.select("#beginText").insert("span").text(">>> Shveta Shah <<<");
 }  
 
 function scene0() {
@@ -48,12 +43,12 @@ function scene0() {
     d3.select("#scene3").selectAll("*").remove();
     d3.select("#scene4").selectAll("*").remove();
     d3.select(".dropdown").classed("hidden",true);
-    //d3.select("#scene0").insert("h2").text("Introduction");
-    d3.select("#scene0").insert("span").text("This narrative visualization shows Food Inspection at Illinois from 2010 till now.");
-    d3.select("#scene0").insert("span").text("Data source is from City of Chicago portal and the data is from 01/01/2010 till now");
+    d3.select("#scene0").insert("h2").text("Introduction");
+    d3.select("#scene0").insert("span").text("This narrative visualization shows crimes occured in Chicago in the year 2020. ");
+    d3.select("#scene0").insert("span").text("Data source is from City of Chicago portal and the data is 01/01/2020 to 12/31/2020");
     d3.select("#scene0").insert("br");
     d3.select("#scene0").insert("br");
-    d3.select("#scene0").insert("span").html('<a target="_blank" href="https://data.cityofchicago.org/Health-Human-Services/Food-Inspections/4ijn-s7e5/data">Link to Food Inspection at Illinois portal</a>');
+    d3.select("#scene0").insert("span").html('<a target="_blank" href="https://data.cityofchicago.org/Public-Safety/Crimes-2020/qzdf-xmn8/data">Link to City of Chicago portal</a>');
     d3.select("#scene0").insert("br");
     d3.select("#scene0").insert("br");
     d3.select("#beginText").insert("span").text("Clicking on next button will show charts containing different scenes");
@@ -67,13 +62,12 @@ function scene1() {
     d3.select("#scene3").selectAll("*").remove();
     d3.select("#scene4").selectAll("*").remove();
     d3.select(".dropdown").classed("hidden",true);
-    d3.select("#scene1").insert("h2").text("Top 10 Facility with High Risk");
-    d3.select("#scene1").insert("span").text("The bar chart shows top 10 different facility with high risk found in Illinois in the year 2022.");
+    d3.select("#scene1").insert("h2").text("Top 10 Types of Crimes");
+    d3.select("#scene1").insert("span").text("The bar chart shows top 10 different types of crimes occurred in Chicago in the year 2020.");
     d3.select("#scene1").insert("br");
     d3.select("#scene1").insert("br");
-    d3.select("#scene1").insert("span").text("Restaurant and School were the most risky facility as per yearly food inspection; on other hand, Liquor, Bar & Church were the least risky facilities");
+    d3.select("#scene1").insert("span").text("Battery and Theft were the most crimes happened and in contract, burglary, weapons violation and robbery crimes occured the least compared to top 7 crimes");
 
-    
     const countUnique = PrimaryTypeArray => {
         const counts = {};
         for (var i = 0; i < PrimaryTypeArray.length; i++) {
@@ -92,10 +86,10 @@ function scene1() {
     });
     
     var arr = items.slice(0, 10);
-    
+
     var x = d3.scaleBand()
             .range([0, 800])
-            .padding(0.5);
+            .padding(0.8);
     var y = d3.scaleLinear()
             .range([500, 0]);
 
@@ -109,7 +103,7 @@ function scene1() {
     .selectAll(".bar")
     .data(arr)
     .enter().append("rect")
-    // .attr("class", "bar")
+    .attr("class", "bar")
     .attr("x", function(d) { return x(d[0]); })
     .attr("width", (5+x.bandwidth()))
     .attr("y", function(d) { return y(d[1]); })
@@ -135,7 +129,7 @@ function scene1() {
     .attr("x",-215)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("NUMBER OF FACILITIES");  
+    .text("NUMBER OF CRIMES");  
 
     d3.select("svg")
     .append("text")
@@ -144,7 +138,7 @@ function scene1() {
     .attr("x", 100)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("FACILITY TYPES");  
+    .text("TYPES OF CRIMES");  
     
 //HIGER
     //DRAW BOX
@@ -164,7 +158,7 @@ function scene1() {
     .attr("y",15)
     .style("font-size","11px")
     .attr("dy",".35em")
-    .text("Facility with Highest Risk:")
+    .text("Highest frequency of Crimes:")
     .attr("fill","black");
 		
 	d3.select("svg")
@@ -173,7 +167,7 @@ function scene1() {
     .attr("y",25)
     .attr("dy",".35em")
     .style("font-size","11px")
-    .text("Restaurant, School & Grocery Store")
+    .text("Burglary, Weapons Violation & Robbery")
     .attr("fill","black");
 
     //DRAW  LINE 
@@ -213,7 +207,7 @@ function scene1() {
     .attr("y",240)
     .style("font-size","11px")
     .attr("dy",".35em")
-    .text("Facility with Low Risk:")
+    .text("Lowest frequency of Crimes:")
     .attr("fill","black");
 		
 	d3.select("svg")
@@ -222,7 +216,7 @@ function scene1() {
     .attr("y",250)
     .attr("dy",".35em")
     .style("font-size","11px")
-    .text("Liquor, Bar & Church")
+    .text("Burglary, Weapons Violation & Robbery")
     .attr("fill","black");
 
     //DRAW  LINE 
@@ -262,11 +256,11 @@ async function scene2() {
     d3.select("#scene3").selectAll("*").remove();
     d3.select("#scene4").selectAll("*").remove();
     d3.select(".dropdown").classed("hidden",true);
-    d3.select("#scene2").insert("h2").text("High Risk Cities");
-    d3.select("#scene2").insert("span").text("The bar chart shows total high risk occurred in Illnois in the year 2021.");
+    d3.select("#scene2").insert("h2").text("Top 10 Crimes Location");
+    d3.select("#scene2").insert("span").text("The bar chart shows total crimes occurred at locations in Chicago in the year 2020.");
     d3.select("#scene2").insert("br");
     d3.select("#scene2").insert("br");
-    d3.select("#scene2").insert("span").text("There is a significant different facilities involved in different risk occured in Illinois (where it is highest) compare to ICE CREAM SHOP & pubs(where total risk is lowest from top 8 locations.)");
+    d3.select("#scene2").insert("span").text("There is a significant different total crimes occured in Street (where it is highest) compare to restaurant & gas station(where total crimes are lowest from top 8 locations.)");
 
     const countUnique = LocationDescriptionArray => {
         const counts = {};
@@ -342,7 +336,7 @@ async function scene2() {
     .attr("y",15)
     .style("font-size","11px")
     .attr("dy",".35em")
-    .text("City with the highest risk as per food inspection portal:")
+    .text("Highest frequency of Crimes:")
     .attr("fill","black");
 		
 	d3.select("svg")
@@ -351,7 +345,7 @@ async function scene2() {
     .attr("y",25)
     .attr("dy",".35em")
     .style("font-size","11px")
-    .text("Chicago")
+    .text("Street")
     .attr("fill","black");
 
     //DRAW  LINE 
@@ -382,7 +376,7 @@ async function scene2() {
     .attr("y",315)
     .style("font-size","11px")
     .attr("dy",".35em")
-    .text("City with low risk as per food inspection portal :")
+    .text("Lowest frequency of Crimes:")
     .attr("fill","black");
 		
 	d3.select("svg")
@@ -391,7 +385,7 @@ async function scene2() {
     .attr("y",325)
     .attr("dy",".35em")
     .style("font-size","11px")
-    .text("ELK GROVE VILLAGE")
+    .text("Restaurant & Gas Station")
     .attr("fill","black");
 
     //DRAW  LINE 
@@ -420,7 +414,7 @@ async function scene2() {
     .attr("x",-215)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("LOCATIONS WITH HIGH RISK ");  
+    .text("NUMBER OF CRIMES");  
 
     d3.select("svg")
     .append("text")
@@ -557,7 +551,6 @@ async function scene3() {
     .attr("stroke","gray");
 }
 
-
 function scene4(ddi) {
     d3.select(".svg").selectAll("*").remove();
     d3.select("#scene0").selectAll("*").remove();
@@ -566,13 +559,13 @@ function scene4(ddi) {
     d3.select("#scene3").selectAll("*").remove();
     d3.select("#scene4").selectAll("*").remove();
     d3.select("#scene3").insert("h2").text("Discover yourself");
-    d3.select("#scene3").insert("span").text("Select any facility type from the dropdown and see the locations. Also sorting graph is enable");
+    d3.select("#scene3").insert("span").text("You can select any crime from the dropdown and see the top 10 locations the selected crime occurs the most. You can sort the graph by clicking on 'Sort this graph' button");
     
     d3.select(".dropdown").classed("hidden",false);
 
     var uniqueCrimesType= crimesType.filter((item, i, ar) => ar.indexOf(item) === i);
 
-    d3.select(".facility").selectAll("option")
+    d3.select(".crime").selectAll("option")
 			.data(uniqueCrimesType)
 			.enter().append("option")
 			.text(d => d);
@@ -647,7 +640,7 @@ function scene4(ddi) {
     .attr("x",-215)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("TOTAL FACILITIES");  
+    .text("NUMBER OF CRIMES");  
 
     d3.select("svg")
     .append("text")
@@ -656,9 +649,9 @@ function scene4(ddi) {
     .attr("x", 100)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("FACILITY TYPPES");  
+    .text("TYPES OF CRIMES");  
     
-    var select = d3.select(".facility")
+    var select = d3.select(".crime")
     .on("change", function() {
         chosenField = this.value;
         scene4(this.value);
@@ -674,13 +667,13 @@ function scene4Sort(ddi) {
     d3.select("#scene3").selectAll("*").remove();
     d3.select("#scene4").selectAll("*").remove();
     d3.select("#scene3").insert("h2").text("Discover yourself");
-    d3.select("#scene3").insert("span").text("You can select any FACILITY from the dropdown and see the top 10 facilities with high risk in Illinois. You can sort the graph by clicking on 'Sort this graph' button");
+    d3.select("#scene3").insert("span").text("You can select any crime from the dropdown and see the top 10 locations the selected crime occurs the most. You can sort the graph by clicking on 'Sort this graph' button");
 
     d3.select(".dropdown").classed("hidden",false);
 
     var uniqueCrimesType= crimesType.filter((item, i, ar) => ar.indexOf(item) === i);
 
-    d3.select(".facility").selectAll("option")
+    d3.select(".crime").selectAll("option")
 			.data(uniqueCrimesType)
 			.enter().append("option")
 			.text(d => d);
@@ -758,7 +751,7 @@ function scene4Sort(ddi) {
     .attr("x",-215)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("Number of facility got inspect every year");  
+    .text("NUMBER OF CRIMES");  
 
     d3.select("svg")
     .append("text")
@@ -767,9 +760,9 @@ function scene4Sort(ddi) {
     .attr("x", 100)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("Facility Types");  
+    .text("TYPES OF CRIMES");  
     
-    var select = d3.select(".facility")
+    var select = d3.select(".crime")
     .on("change", function() {
         chosenField = this.value;
         scene4(this.value);
